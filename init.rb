6ef67dcd6b43ccbcf2af3fa7242b08527c2decf4
Joe Fiorini <joe@faithfulgeek.org>
2010-06-26 20:29:52 -0400

    load_once_paths renamed to autoload_once_paths
    Commit 6f83a5036d8a9c3f8ed74755ff6d42bc3f6e9982
    in the Rails project made this change.

--- init.rb
+++ init.rb
@@ -1,5 +1,5 @@
 
 # Since MockupsController subclasses (and thus loads) ApplicationController, we need to make sure that
 # it gets reloaded in development. Without this, things will break after the first reload.
-ActiveSupport::Dependencies.load_once_paths.delete(File.expand_path(File.dirname(__FILE__))+'/app/controllers')
+ActiveSupport::Dependencies.autoload_once_paths.delete(File.expand_path(File.dirname(__FILE__))+'/app/controllers')
 
