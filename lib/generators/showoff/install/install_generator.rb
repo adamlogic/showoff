968e571dc2c7f9a9d28e57f85323c06e5302dbb1
Joe Fiorini <joe@faithfulgeek.org>
2010-06-26 21:56:06 -0400

    Rails3 compatible generator
    Changes routes from being part of plugin to
    generated in config/routes.rb. The curent Rails3
    recommendation for routes is not finalized. This
    should be updated once the Rails team has a better
    tie-in for routes.

---
+++ lib/generators/showoff/install/install_generator.rb
@@ -0,0 +1,26 @@
+module Showoff
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      source_root File.expand_path(File.dirname(__FILE__) + '/templates')
+
+
+      def install_showoff
+        empty_directory 'app/views/mockups/basic'
+
+        copy_file 'sample_mockup.html.erb', 'app/views/mockups/sample_mockup.html.erb'
+        copy_file 'alternate_layout.html.erb', 'app/views/mockups/basic/alternate_layout.html.erb'
+        copy_file 'showoff.css', 'public/stylesheets/showoff.css'
+
+      end
+
+
+      def create_routes
+        route "match '/mockups', :to => 'mockups#frameset', :as => 'mockups'"
+        route "match '/mockups_index', :to => 'mockups#index', :as => 'mockups_index'"
+        route "match '/mockup/:template_name', :to => 'mockups#show', :as => 'mockup'"
+      end
+
+
+    end
+  end
+end
